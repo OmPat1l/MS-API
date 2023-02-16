591ce904f336c440c84e879c1c1014ddbec6d8da
Om Patil <omajitsinhpatil@gmail.com>
2023-02-16 21:02:58 +0530

    first

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+// const morgan = require("morgan");
+const app = express();
+const fs = require("fs");
+let data1 = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
+let pdfNumber = 202310001;
+// app.use(morgan("dev"));
+app.use(express.json());
+app.get("/api/v1/data", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      data1,
+    },
+  });
+});
+app.post("/api/v1/data", (req, res) => {
+  var currentDate = new Date();
+  var dateString = currentDate.toLocaleDateString();
+  var timeString = currentDate.toLocaleTimeString();
+  const newId = data1[data1.length - 1].id + 1;
+  pdfNumber = pdfNumber + 1;
+  const newData = Object.assign(
+    { id: newId },
+    req.body,
+    { pdf: pdfNumber },
+    { data: dateString },
+    { time: timeString }
+  );
+  data1.push(newData);
+  fs.writeFile(`${__dirname}/data.json`, JSON.stringify(data1), (err) => {
+    // if (err) {
+    //   res.status(404).json({
+    //     status: "fail",
+    //   });
+    // }
+    let data1 = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
+    let buff = data1[data1.length - 1];
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        buff,
+      },
+    });
+  });
+
+  //   app.patch("/api/v1/tours/:x", (req, res) => {
+  //     const buff = req.params.x * 1;
+  //     if (buff > tours.length - 1) {
+  //       res.status(404).json({
+  //         status: "fail",
+  //         tour: "invalid",
+  //       });
+  //     }
+  //   });
+
+  //   app.get("/api/v1/tours/:x", (req, res) => {
+  //     const id = parseInt(req.params.x, 10);
+  //     let tour = tours[0].id;
+  //     // const tour = tours.find((el) => el.id === id);
+  //     for (let i = 0; i < tours.length; i++) {
+  //       if (tours[i].id === id) {
+  //         tour = tours[i].id;
+  //         return;
+  //       }
+  //     }
+  //     if (!tour) {
+  //       return res.status(404).json({
+  //         status: "fail",
+  //         message: "invalid id",
+  //       });
+  //     }
+  //     res.status(200).json({
+  //       status: "success",
+  //       data: {
+  //         tour,
+  //       },
+  //     });
+  //   });
+
+  // const name1=req.name;
+  // res.send("done");
+  // res.status(200).json({
+
+  //     "name":name1
+  // }
+  // );
+});
+
+app.listen(3001);
