dce45fe2a04da259ddad57fa785907413acb305d
Om Patil <omajitsinhpatil@gmail.com>
2023-02-19 14:31:36 +0530

    otp added

--- MailAuthentication/app.js
+++ MailAuthentication/app.js
@@ -5,8 +5,6 @@ const nodemailer = require("nodemailer");
 // const sendMail=require('./')
 let data1 = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
 let datag = JSON.parse(fs.readFileSync(`${__dirname}/generaldata.json`));
-let otp = Math.floor(Math.random() * 1000000);
-otp = otp.toString().padStart(6, "0");
 
 app.use(express.json());
 //get req not for users
@@ -37,31 +35,20 @@ app.post("/mindspark/v1/data", (req, res) => {
     if (inputMis == datamis && inputMail == datamail) {
       let buffprime = datag.totalpasses;
       if (buffprime > 0) {
+        let otp = Math.floor(Math.random() * 1000000);
+        otp = otp.toString().padStart(6, "0");
         buffprime--;
         let buffObject = { totalpasses: buffprime };
         Object.assign(datag, buffObject);
         const updatedJsonString = JSON.stringify(datag);
         fs.writeFileSync(`${__dirname}/generaldata.json`, updatedJsonString);
+        res.status(201).json({
+          status: "success",
+          url: "http://localhost:3002/mindspark/v1/data/verified",
+          otp: `${otp}`,
+        });
 
         //sending mail
-        const sendMail = async (req, res) => {
-          let testAccount = await nodemailer.createTestAccount();
-          const transporter = await nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            auth: {
-              user: "haven.weissnat35@ethereal.email",
-              pass: "rDg6ruEKhkgVxFyMn1",
-            },
-          });
-          let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <ompatil@mindspark.com>', // sender address
-            to: `${datamail}`, // list of receivers
-            subject: "MindSpark OTP", // Subject line
-            text: `click on the given link to authorize your service and enter ${otp}`, // plain text body
-            // html body
-          });
-        };
 
         return;
       } else {
